fcb254c448ff6a7badb37af895e1fa989833288b
z-aitkourdas <zouhair.blackboy@gmail.com>
2020-12-01 21:35:24 +0100

    Adding comments the client code

--- Client/client.py
+++ Client/client.py
@@ -4,26 +4,34 @@ def Main():
 	host = str(input("Enter Server name : "))
 	port = 5000
 
+	# Create a socket object
 	s = socket.socket()
-	s.connect((host,port))
+	s.connect((host,port))  # Connect to the server
 
 	print('Connected!')
-	filename = input('File to download ? => ')
+	filename = input('File to download ? => ')  # File to download
 	if filename != '' :
+		# Sending the request
 		s.send(bytes(filename,'utf-8'))
+		
+		# Getting the response from the server
 		data = s.recv(1024)
 		data = data.decode()
-		print('data :', data)
-		if data[:5] == 'EXIST' :
+
+		if data[:5] == 'EXIST' :  # Check if the file exist or not
+			# Get the file size from the response
 			filesize = data[5:]
-			message = input('File Exists, ' + str(filesize) + \
-				                'Bytes, download? (Y/N) -> ')
-			if message.lower() == 'y' :
-				s.send(bytes('OK', 'utf-8'))
-				f = open('Downloaded' + str(filename), 'wb')
+			message = input('File Exists, ' + str(filesize) + 'Bytes, download? (Y/N) -> ')
+
+			if message.lower() in ['y', 'yes'] :  # Check if the client want to continue the download
+				s.send(bytes('OK', 'utf-8'))  # To let the server that we want to dowanload the file
+				
+				f = open('Downloaded_' + str(filename), 'wb')  # Create the file that we want
+				
+				# Recieve the data from the server
 				data = s.recv(1024)
 				dataRecv = len(data)
-				f.write(data)
+				f.write(data)  # Write the recieved data
 				while dataRecv < int(filesize) :
 					data = s.recv(10000000)
 					dataRecv += len(data)
