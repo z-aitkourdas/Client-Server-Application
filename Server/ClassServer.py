1f48cedf488660b5498b3bd2fa201e4aa648b098
z-aitkourdas <zouhair.blackboy@gmail.com>
2020-12-03 22:13:05 +0100

    Fix the placement of 1st,2nd lignes in sendFile

--- Server/ClassServer.py
+++ Server/ClassServer.py
@@ -43,11 +43,12 @@ class Server:
             return None
  
     def sendFile(self, name, connection):
-        # file_to_send = connection.recv(1024)
-        # file_to_send = file_to_send.decode()
 
         self.listFiles(connection)
 
+        file_to_send = connection.recv(1024)
+        file_to_send = file_to_send.decode()
+        
         is_file = self.isFile(file_to_send)
 
         if is_file != None:
