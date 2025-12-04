4bca17430ced68f189ae69908ed33d843e378e14
AIT KOURDS ZOUHAIR <39438182+z-aitkourdas@users.noreply.github.com>
2025-12-04 11:30:29 +0100

    Optimize client-server file transfer handling

--- Client/client.py
+++ Client/client.py
@@ -1,47 +1,49 @@
 import socket
+from pathlib import Path
 
-def Main():
-	host = str(input("Enter Server name : "))
-	port = 5000
+BUFFER_SIZE = 8192
 
-	# Create a socket object
-	s = socket.socket()
-	s.connect((host,port))  # Connect to the server
 
-	print('Connected!')
-	filename = input('File to download ? => ')  # File to download
-	if filename != '' :
-		# Sending the request
-		s.send(bytes(filename,'utf-8'))
-		
-		# Getting the response from the server
-		data = s.recv(1024)
-		data = data.decode()
+def download_file(sock: socket.socket, filename: str, filesize: int) -> None:
+    target_path = Path.cwd() / f"Downloaded_{filename}"
+    with target_path.open("wb") as target:
+        received = 0
+        while received < filesize:
+            chunk = sock.recv(BUFFER_SIZE)
+            if not chunk:
+                break
+            target.write(chunk)
+            received += len(chunk)
+    if received >= filesize:
+        print("The file has been completely transferred.")
+    else:
+        print("Download interrupted before completion.")
 
-		if data[:5] == 'EXIST' :  # Check if the file exist or not
-			# Get the file size from the response
-			filesize = data[5:]
-			message = input('File Exists, ' + str(filesize) + 'Bytes, download? (Y/N) -> ')
 
-			if message.lower() in ['y', 'yes'] :  # Check if the client want to continue the download
-				s.send(bytes('OK', 'utf-8'))  # To let the server that we want to dowanload the file
-				
-				f = open('Downloaded_' + str(filename), 'wb')  # Create the file that we want
-				
-				# Recieve the data from the server
-				data = s.recv(1024)
-				dataRecv = len(data)
-				f.write(data)  # Write the recieved data
-				while dataRecv < int(filesize) :
-					data = s.recv(10000000)
-					dataRecv += len(data)
-					f.write(data)
-					print(dataRecv)
-				f.close()
-				print ('The file has been completely transfered ')
-		else :
-			print('File does not Exist!')
-	s.close()
+def main() -> None:
+    host = input("Enter Server name : ")
+    port = 5000
 
-if __name__ == '__main__' :
-        Main()
+    with socket.create_connection((host, port)) as sock:
+        print("Connected!")
+        filename = input("File to download ? => ")
+        if not filename:
+            return
+
+        sock.sendall(filename.encode())
+        response = sock.recv(BUFFER_SIZE).decode()
+        if not response.startswith("EXIST"):
+            print("File does not Exist!")
+            return
+
+        filesize = int(response[5:])
+        message = input(f"File Exists, {filesize} Bytes, download? (Y/N) -> ")
+        if message.lower() not in {"y", "yes"}:
+            return
+
+        sock.sendall(b"OK")
+        download_file(sock, filename, filesize)
+
+
+if __name__ == "__main__":
+    main()
